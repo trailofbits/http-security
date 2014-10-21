6ab1f94a5e5c75b34e28fec4b14c1b9e27180604
Dominic Owen <you@example.com>
2014-10-21 15:42:57 -0400

    Add Content-Security-Policy

--- lib/security_headers/parser.rb
+++ lib/security_headers/parser.rb
@@ -18,7 +18,8 @@ module SecurityHeaders
       cache_control                     |
       pragma                            |
       expires                           |
-      x_permitted_cross_domain_policies
+      x_permitted_cross_domain_policies |
+      content_security_policy
     end
 
     def self.header_to_sym(header)
@@ -135,6 +136,31 @@ module SecurityHeaders
       stri("all")
     end
 
+    # Content-Security-Policy
+    # Syntax:
+    # Content-Security-Policy =
+    # policy-token    = [ directive-token *( ";" [ directive-token ] ) ]
+    # directive-token = *WSP [ directive-name [ WSP directive-value ] ]
+    # directive-name  = 1*( ALPHA / DIGIT / "-" )
+    # directive-value = *( WSP / <VCHAR except ";" and ","> )
+    #
+    # Parsing Policies:
+    # To parse the policy policy, the user agent MUST use an algorithm equivalent to the following:
+    #   1. Let the set of directives be the empty set.
+    #   2. For each non-empty token returned by strictly splitting the string policy on the character U+003B SEMICOLON (;):
+    #     1. Skip whitespace.
+    #     2. Collect a sequence of characters that are not space characters. The collected characters are the directive name.
+    #     3. If there are characters remaining in token, skip ahead exactly one character (which must be a space character).
+    #     4. The remaining characters in token (if any) are the directive value.
+    #     5. If the set of directives already contains a directive whose name is a case insensitive match for directive name,
+    #        ignore this instance of the directive and continue to the next token.
+    #     6. Add a directive to the set of directives with name directive name and value directive value.
+    #   3. Return the set of directives.
+    # TODO: avoid duplicates (step 2.5)
+    header_rule("Content-Security-Policy") do
+      csp_directive >> wsp >> csp_value_sequence >> ( str(";") >> wsp >>
+        csp_directive >> wsp >> csp_value_sequence ).repeat(0) >> semicolon.maybe
+    end
 
     # Pragma
     # Syntax:
@@ -327,6 +353,27 @@ module SecurityHeaders
     rule(:wsp) { str(" ") | str("\t") }
     rule(:lws) { match[" \t"] }
     rule(:wsp?) { lws.repeat }
+
+    #
+    # CSP Helpers
+    #
+    rule(:csp_value_sequence) { csp_value >> (wsp >> csp_value).repeat(0) }
+
+    rule(:csp_directive) { csp_directive_char.repeat }
+    rule(:csp_value) { csp_value_char.repeat }
+
+    #CSP tokens are any character except space, comma and semicolon
+    #TODO blacklist bad chars instead of whitelist valid chars
+    rule(:csp_directive_char) do
+      alpha | digit | str("-")
+    end
+
+    rule(:csp_value_char) do
+      match["\x00-\x1f"] |
+      match["\x21-\x2b"] |
+      match["\x2d-\x3a"] |
+      match["\x3c-\x7f"]
+    end
 
     #
     # URI Elements
