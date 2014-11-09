1b170482ffe03b0de47d932b539b90ddf1144654
Dominic Owen <you@example.com>
2014-11-09 18:27:09 -0500

    Add rule specific date tests

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -2,8 +2,12 @@ require "spec_helper"
 require "security_headers/parser"
 require 'curb'
 
+class DateParser < Parslet::Parser
+  rule(:http_date) { str('date') }
+end
+
 describe Parser do
-    describe "General parsing" do
+  describe "General parsing" do
     subject { described_class.new.security_headers }
 
     it "parses excess whitespace" do
@@ -105,7 +109,22 @@ describe Parser do
         {excluded: "Date: Sun, 02 Nov 2014 19:31:58 GMT"},
       ])
     end
+  end
 
+  describe "General date parsing" do
+    subject { described_class.new.http_date }
+    it "parses rfc1123-date" do
+      date = "Thu, 04 Dec 2015 16:00:00 GMT"
+      expect(subject.parse(date)).to eq(date)
+    end
+    it "parses rfc850-date" do
+      date = "Thursday, 04-Dec-15 16:00:00 GMT"
+      expect(subject.parse(date)).to eq(date)
+    end
+    it "parses rfc1123-date" do
+      date = "Thu Dec 04 16:00:00 2015"
+      expect(subject.parse(date)).to eq(date)
+    end
   end
 
   describe "X-Frames-Options" do
