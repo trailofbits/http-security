1fee5aa2a39b6581e227e53d9b07abe4cec57459
Postmodern <postmodern.mod3@gmail.com>
2014-11-19 16:39:38 -0800

    Only load codeclimate if the token is set.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
+if ENV['CODECLIMATE_REPO_TOKEN']
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
+end
 
 require 'rspec'
 require 'security_headers/version'
