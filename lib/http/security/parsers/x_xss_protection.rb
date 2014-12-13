bb0ed827ad99d0ffc2fed891611db042a4564670
Postmodern <postmodern.mod3@gmail.com>
2014-12-12 21:47:22 -0800

    Add support for the report= directive in X-Xss-Protection (closes #18).
    * https://bugs.webkit.org/show_bug.cgi?id=100892
    * https://code.google.com/p/chromium/codesearch#chromium/src/third_party/WebKit/Source/platform/network/HTTPParsers.cpp&l=355

--- lib/http/security/parsers/x_xss_protection.rb
+++ lib/http/security/parsers/x_xss_protection.rb
@@ -9,13 +9,17 @@ module HTTP
         # X-Content-Type-Options: < 1 | 0 >
         #                         /; mode=block
         rule(:x_xss_protection) do
-          x_xss_flag >> (semicolon >> x_xss_mode).maybe
+          x_xss_flag >> (semicolon >> x_xss_mode).maybe >> (semicolon >> x_xss_report).maybe
         end
         root :x_xss_protection
 
         rule(:x_xss_flag) { match['01'].as(:boolean).as(:enabled) }
         rule(:x_xss_mode) do
           stri("mode") >> equals >> stri("block").as(:mode)
+        end
+
+        rule(:x_xss_report) do
+          stri('report') >> equals >> any.repeat(1).as(:report)
         end
       end
     end
--- spec/parsers/x_xss_protection_spec.rb
+++ spec/parsers/x_xss_protection_spec.rb
@@ -2,6 +2,18 @@ require "spec_helper"
 require "http/security/parsers/x_xss_protection"
 
 describe Parsers::XXSSProtection do
+  it "it accepts 1" do
+    header = "1"
+
+    expect(subject.parse(header)).to eq(enabled: true)
+  end
+
+  it "it accepts 0" do
+    header = "0"
+
+    expect(subject.parse(header)).to eq(enabled: false)
+  end
+
   it "it accepts 1; mode=block" do
     header = "1; mode=block"
 
@@ -14,15 +26,14 @@ describe Parsers::XXSSProtection do
     expect(subject.parse(header)).to eq(enabled: false, mode: 'block')
   end
 
-  it "it accepts 1" do
-    header = "1"
+  it "it accepts 1; mode=block; report=..." do
+    report_uri = "/xss-report/25b8988e-64ff-45a8-b0c6-2700fc1e9abd?source%5Baction%5D=index&source%5Bcontroller%5D=shop&source%5Bsection%5D=storefront"
+    header     = "1; mode=block; report=#{report_uri}"
 
-    expect(subject.parse(header)).to eq(enabled: true)
-  end
-
-  it "it accepts 0" do
-    header = "0"
-
-    expect(subject.parse(header)).to eq(enabled: false)
+    expect(subject.parse(header)).to eq(
+      enabled: true,
+      mode: 'block',
+      report: report_uri
+    )
   end
 end
