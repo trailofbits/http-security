ce145fbb82fd18aa2acc5b6d47802bc991c3a51f
Postmodern <postmodern.mod3@gmail.com>
2014-12-19 15:20:11 -0800

    Added specs for the SetCookie parser.

---
+++ spec/parsers/set_cookie_spec.rb
@@ -0,0 +1,34 @@
+require "spec_helper"
+require "http/security/parsers/set_cookie"
+
+describe Parsers::SetCookie do
+  it "accepts 'name=value'" do
+    expect(subject.parse('foo=bar')).to be == {
+      cookie: {foo: 'bar'}
+    }
+  end
+
+  it "accepts 'name=value; Expires=...'" do
+    expires = "Wed, 09 Jun 2021 10:18:14 GMT"
+
+    expect(subject.parse("foo=bar; Expires=#{expires}")).to be == [
+      {cookie: {foo: 'bar'}},
+      {expires: expires}
+    ]
+  end
+
+  it "accepts 'name=value; Path=...; Expires=...; Secure; Domain=...; HttpOnly'" do
+    path    = '/accounts'
+    expires = "Wed, 09 Jun 2021 10:18:14 GMT"
+    domain  = '.example.com'
+
+    expect(subject.parse("foo=bar; Path=#{path}; Expires=#{expires}; Secure; Domain=#{domain}; HttpOnly")).to be == [
+      {cookie: {foo: 'bar'}},
+      {path: path},
+      {expires: expires},
+      {secure: 'Secure'},
+      {domain: domain},
+      {http_only: 'HttpOnly'}
+    ]
+  end
+end
