c359e7f20a2daeb5028abd736bbc153389abacf7
Postmodern <postmodern.mod3@gmail.com>
2015-01-23 17:22:12 -0800

    Added specs for Response.parse_header, .parse and .parse!.

--- spec/response_spec.rb
+++ spec/response_spec.rb
@@ -2,10 +2,35 @@ require "spec_helper"
 require "http/security/response"
 
 describe Response do
+  subject { described_class }
+
   describe "#initialize" do
     subject do
       described_class.new(
       )
+    end
+  end
+
+  describe ".parse_header" do
+    let(:name)  { 'Set-Cookie' }
+    let(:value) { "_twitter_sess=BAh7CSIKZmxhc2hJQzonQWN0aW9uQ29udHJvbGxlcjo6Rmxhc2g6OkZsYXNo%250ASGFzaHsABjoKQHVzZWR7ADoPY3JlYXRlZF9hdGwrCOzcmMpJAToMY3NyZl9p%250AZCIlYmEzNTQ5YzM0MzYwZjAzZWMwMTFmZDY3MzVhMjE0MzM6B2lkIiUxMzI3%250AY2M1OWIyYzM3N2IzMmYxZWZiNmJlN2ZmYzdjZQ%253D%253D--09c51d06332d2b4cf102948a3f0491131ed952fa; Path=/; Domain=.twitter.com; Secure; HTTPOnly, guest_id=v1%3A141644325604142464; Domain=.twitter.com; Path=/; Expires=Sat, 19 Nov 2016 00:27:36 GMT" }
+
+    it "should parse the given headre" do
+      expect(subject.parse_header(name,value)).to be == [
+        {
+          cookie:    {:_twitter_sess=>"BAh7CSIKZmxhc2hJQzonQWN0aW9uQ29udHJvbGxlcjo6Rmxhc2g6OkZsYXNo%250ASGFzaHsABjoKQHVzZWR7ADoPY3JlYXRlZF9hdGwrCOzcmMpJAToMY3NyZl9p%250AZCIlYmEzNTQ5YzM0MzYwZjAzZWMwMTFmZDY3MzVhMjE0MzM6B2lkIiUxMzI3%250AY2M1OWIyYzM3N2IzMmYxZWZiNmJlN2ZmYzdjZQ%253D%253D--09c51d06332d2b4cf102948a3f0491131ed952fa"},
+          path:      "/",
+          domain:    ".twitter.com",
+          secure:    "Secure",
+          http_only: "HTTPOnly"
+        },
+        {
+          cookie:  {:guest_id=>"v1%3A141644325604142464"},
+          domain:  ".twitter.com",
+          path:    "/",
+          expires: Date.parse('Sat, 19 Nov 2016 00:27:36 GMT')
+        }
+      ]
     end
   end
 
@@ -83,6 +108,34 @@ describe Response do
       expect(subject.x_xss_protection).to_not be_nil
     end
 
+    context "when parsing a malformed headers" do
+      let(:response) do
+        {
+          "Date"=>"Sat, 24 Jan 2015 01:06:57 GMT",
+          "X-Servedby"=>"ny1-prod6-web022.int.peer1.squarespace.net",
+          "Set-Cookie"=>
+          "JSESSIONID=3bGO1nRYPl_-0xhNbk8nlpt-5ulKdHw9_ofbsM_wsJ-Wcul2ODOLqQ;Path=/;HttpOnly, crumb=ce3ba8cfc3;Path=/, SS_MID=0763cce4-5adf-4332-a545-f5b4ebefd803i5aarqtx;Path=/;Domain=.singularads.com;Expires=Tue, 21-Jan-2025 01:06:57 GMT",
+          "Expires"=>"Thu, 01 Jan 1970 00:00:00 GMT",
+          "Accept-Ranges"=>"bytes",
+          "Content-Type"=>"text/html; charset=UTF-8",
+          "X-Pc-Appver"=>"3070",
+          "Content-Encoding"=>"gzip",
+          "X-Pc-Date"=>"Fri, 23 Jan 2015 21:00:13 GMT",
+          "X-Pc-Host"=>"10.100.101.11",
+          "Etag"=>"W/\"0eaeb395a0f1a569174a067510085a66\"",
+          "X-Pc-Key"=>"m7ZrcPWNTQjYc6jL7aW2oRSLPko-daniel-matalon-vim3",
+          "X-Pc-Hit"=>"true",
+          "Content-Length"=>"27469",
+          "X-Contextid"=>"nLOTffVs/ksjmZDEy",
+          "X-Via"=>"1.1 ny1-prod-echo016.int.peer1.squarespace.net"
+        }
+      end
+
+      it "should map exceptions to MalformedHeader objects" do
+        expect(subject.set_cookie).to be_kind_of(MalformedHeader)
+      end
+    end
+
     context "Alexa 100", :gauntlet do
       require 'csv'
       require 'net/http'
@@ -106,6 +159,38 @@ describe Response do
             end
           end
         end
+      end
+    end
+  end
+
+  describe ".parse!" do
+    let(:response) do
+      {
+        "Date"=>"Sat, 24 Jan 2015 01:06:57 GMT",
+        "X-Servedby"=>"ny1-prod6-web022.int.peer1.squarespace.net",
+        "Set-Cookie"=>
+        "JSESSIONID=3bGO1nRYPl_-0xhNbk8nlpt-5ulKdHw9_ofbsM_wsJ-Wcul2ODOLqQ;Path=/;HttpOnly, crumb=ce3ba8cfc3;Path=/, SS_MID=0763cce4-5adf-4332-a545-f5b4ebefd803i5aarqtx;Path=/;Domain=.singularads.com;Expires=Tue, 21-Jan-2025 01:06:57 GMT",
+          "Expires"=>"Thu, 01 Jan 1970 00:00:00 GMT",
+          "Accept-Ranges"=>"bytes",
+          "Content-Type"=>"text/html; charset=UTF-8",
+          "X-Pc-Appver"=>"3070",
+          "Content-Encoding"=>"gzip",
+          "X-Pc-Date"=>"Fri, 23 Jan 2015 21:00:13 GMT",
+          "X-Pc-Host"=>"10.100.101.11",
+          "Etag"=>"W/\"0eaeb395a0f1a569174a067510085a66\"",
+          "X-Pc-Key"=>"m7ZrcPWNTQjYc6jL7aW2oRSLPko-daniel-matalon-vim3",
+          "X-Pc-Hit"=>"true",
+          "Content-Length"=>"27469",
+          "X-Contextid"=>"nLOTffVs/ksjmZDEy",
+          "X-Via"=>"1.1 ny1-prod-echo016.int.peer1.squarespace.net"
+      }
+    end
+
+    context "when parsing malformed headers" do
+      it "should raise a Parslet::ParseFailed exception" do
+        expect {
+          subject.parse!(response)
+        }.to raise_error(Parslet::ParseFailed)
       end
     end
   end
